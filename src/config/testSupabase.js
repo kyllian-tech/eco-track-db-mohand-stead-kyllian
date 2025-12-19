d3eec4dbd768d08702f226a74bfc03b0e827ec0e
kyllian-tech <204902804+projetfilrouge-ingetis@users.noreply.github.com>
2025-12-19 11:28:45 +0100

    Création du microservice ganification : service-gamification

--- src/config/testSupabase.js
+++ src/config/testSupabase.js
@@ -8,7 +8,7 @@ const url = process.env.SUPABASE_URL
 const key = process.env.SUPABASE_ANON_KEY
 
 if (!url || !key) {
-  console.error("❌ Variables d'environnement manquantes")
+  console.error(" Variables d'environnement manquantes")
   process.exit(1)
 }
 
@@ -16,16 +16,16 @@ const supabase = createClient(url, key)
 
 async function main() {
   const { data, error } = await supabase
-    .from('badges') // remplace par une table existante
+    .from('badges') // remplacé par une table existante
     .select('*')
     .limit(1)
 
   if (error) {
-    console.error("❌ Erreur Supabase :", error.message)
+    console.error(" Erreur Supabase :", error.message)
     process.exit(1)
   }
 
-  console.log("✅ Connexion OK ! Données :", data)
+  console.log(" Connexion OK ! Données :", data)
 }
 
 main()
